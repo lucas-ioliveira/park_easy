73e7492d6d1d8aadb358b122e687c4de348e152d
Lucas Oliveira <lucasio2008@gmail.com>
2023-11-15 12:44:05 -0300

    feat(user_auth): Add name in fild.
    Add name in fild for response.

--- user_auth/serializers.py
+++ user_auth/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    
+    job_title_name = serializers.SerializerMethodField()
+
+    def get_job_title_name(self, obj):
+        return obj.get_job_title_display()
