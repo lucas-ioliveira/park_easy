cf17e3a19206f3fe026007f3182df7bfd04fc55c
Lucas Oliveira <lucasio2008@gmail.com>
2023-11-18 18:54:49 -0300

    refactor(clients_parking): Alter name app
    Alter name clients_parking to Clients for apresentation on django admin.

--- clients_parking/apps.py
+++ clients_parking/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ClientsParkingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clients_parking'
+    verbose_name = 'Clients'
