99a424995623b01c5d9526efbbab9be0cfddddd8
Lucas Oliveira <lucasio2008@gmail.com>
2023-11-18 18:58:29 -0300

    refactor(user_auth): Alter name app.
    Alter name user_auth to Employees for apresentation on django admin.

--- user_auth/apps.py
+++ user_auth/apps.py
@@ -5,4 +5,4 @@ class UserAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_auth'
     verbose_name = 'User Auth'
-    verbose_name_plural = 'User Auths'
+    verbose_name_plural = 'Employees'
