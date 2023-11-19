11c31c86976e34609cb979943a1c7590328f96ef
Lucas Oliveira <lucasio2008@gmail.com>
2023-11-19 18:54:19 -0300

    refactor(car): Add name.
    Add method to present car owner name in response.

--- cars/serializers.py
+++ cars/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import Car
-from clients_parking.models import Clients
 
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = '__all__'
+    
+    owner_name = serializers.SerializerMethodField()
+
+    def get_owner_name(self, obj):
+        return obj.owner.first_name
