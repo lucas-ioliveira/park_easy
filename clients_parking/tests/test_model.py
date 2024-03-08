322d9f122c05ab5b5efd0973f17bd9e685893e79
Lucas Oliveira <lucasio2008@gmail.com>
2024-03-07 22:01:40 -0300

    test(park_easy/client_parking): Unit Test.
    Development of the test to create a client in the client model.

---
+++ clients_parking/tests/test_model.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from clients_parking.models import Clients
+from django.utils import timezone
+
+class CarTest(TestCase):
+
+    
+    def test_create_client(self):  
+        Clients.objects.create(
+            first_name = 'teste',
+            last_name = 'teste',
+            email = 'teste@email.com',
+            phone = '11111111111',
+            address = 'rua teste',
+            city = 'rua teste',
+            state = 'rua teste',
+            country = 'rua teste',
+            is_active = True,
+            created_at = timezone.now(),
+            updated_at = timezone.now(),
+        )
+        self.assertEqual(Clients.objects.count(), 1)
